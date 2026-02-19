cbee79f6ab3d9e20bc535767e8d1eced72dfd0de
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-02-19 15:32:17 -0500

    Se agrego la etiqueta <?php ?> al archivo add.php

--- add.php
+++ add.php
@@ -1,7 +1,10 @@
-<form action="add-item.php" method="Post">
-    <input type="text" name="name" placeholder="Nombre">
-    <input type="text" name="marca" placeholder="Marca">
-    <input type="number" name="stock" placeholder="Stock" min=0 max=1000>
-    <input type="number" name="precio" placeholder="Precio" min=0 max=9999>
-    <input type="submit"  value="enviar">
-</form>
+<?php
+    <form action="add-item.php" method="Post">
+        <input type="text" name="name" placeholder="Nombre">
+        <input type="text" name="marca" placeholder="Marca">
+        <input type="number" name="stock" placeholder="Stock" min=0 max=1000>
+        <input type="number" name="precio" placeholder="Precio" min=0 max=9999>
+        <input type="submit"  value="enviar">
+    </form>
+?>
+
